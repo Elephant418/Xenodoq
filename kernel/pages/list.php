2f95d8e574cc75fcb44df01e0c1a7a6797d4f0da
Thomas ZILLIOX <thomas@zilliox.me>
2012-09-02 04:09:07 +0200

    Listing as a table & subfolder

--- kernel/pages/list.php
+++ kernel/pages/list.php
@@ -1,9 +1,49 @@
-<ul>
+<table class="table">
+	<thead>
+		<tr>
+			<th></th>
+			<th>Nom du fichier</th>
+			<th>Taille</th>
+			<th>Date</th>
+		</tr>
+	</thead>
+	<tbody>
 <?php
 $title = 'Liste des fichiers';
-$files = \UFile\list_relative( User::$data_path );
+if ( URI != '/' ) {
+?>
+	<tr>
+		<td><img src="/icon/folder.png" alt="folder icon" /></td>
+		<td><a href="<?= dirname( URI ) ?>"><i>Dossier parent</i></a></td>
+		<td></td>
+		<td></td>
+	</tr>
+<?php
+}
+$files = \UFile\list_folder( User::$data_path );
+$today = gmdate( 'd M Y', time( ) );
 foreach ( $files as $file ) {
-	echo '<li><a href="' . $file . '">' . \UFile\name( $file ) . '</a></li>';
+	$type = \UFile\type( $file );
+	$href = substr( $file, strlen( User::$root_path ) );
+	if ( $type == 'folder' ) {
+		$date = '';
+		$size = '';
+	} else {
+		$date = gmdate( 'd M Y', filemtime( $file ) );
+		if ( $date == $today ) {
+			$date = gmdate( 'H:i:s', filemtime( $file ) );
+		}
+		$size = \UFile\format_size( $file );
+	}
+?>
+	<tr>
+		<td><img src="/icon/<?= $type ?>.png" alt="<?= $type ?> icon" /></td>
+		<td><a href="<?= $href ?>"><?= \UFile\name( $file ) ?></a></td>
+		<td><?= $size ?></td>
+		<td><?= $date ?></td>
+	</tr>
+<?php
 }
 ?>
-</ul>
+	</tbody>
+</table>
--- kernel/utils/File.php
+++ kernel/utils/File.php
@@ -13,23 +13,21 @@ namespace UFile;
   PATH METHODS                   
  *************************************************************************/
 function name( $file_path ) {
-	return \UString\substr_before_last( basename( $file_path ), '.' );
+	$file_name = basename( $file_path );
+	return \UString\substr_before( $file_name, '.' );
 }
 
 function extension( $file_path ) {
-	return \UString\substr_after_last( $file_path, '.' );
-}
-
-function list_all( $path ) {
-	return glob( $path . '/*' );
-}
-
-function list_relative( $path ) {
-	$files = list_all( $path );
-	foreach ( $files as $key => $file ) {
-		$files[ $key ] = substr( $file, strlen( $path ) );
+	$name = basename( $file_path );
+	if ( ! \UString\contains( $name, '.' ) ) {
+		return '';
 	}
-	return $files;
+	return \UString\substr_after_last( $name, '.' );
+}
+
+function list_folder( $path ) {
+	\UString\must_not_ends_with( $path, '/' );
+	return glob( $path . '/*' );
 }
 
 function content_type( $file_path ) {
@@ -65,4 +63,66 @@ function deliver( $file_path ) {
 	header( 'Content-Length: ' . filesize( $file_path ) );
 	readfile( $file_path );
 	exit( );
+}
+
+function type( $file_path ) {
+	switch( strtolower( extension( $file_path ) ) ) {
+		case ''    : return 'folder';
+		case 'rar' : 
+		case 'tar' : 
+		case 'tgz' : 
+		case 'zip' : 
+		case 'gz'  : 
+		case 'bz2' : 
+		case 'cab' :
+		case '7z'  : return 'archive';
+		case 'wav' : 
+		case 'wma' : 
+		case 'mp3' : 
+		case 'mp4' : return 'video'; // 'audio';
+		case 'xml' : 
+		case 'css' : return 'file'; // 'code';
+		case 'sql' : return 'file'; // 'database';
+		case 'bmp' : 
+		case 'xcf' : 
+		case 'psd' : 
+		case 'png' : 
+		case 'jpg' : 
+		case 'jpeg': 
+		case 'gif' : return 'image';
+		case 'pdf' : return 'pdf';
+		case 'xls' : 
+		case 'pps' : 
+		case 'ppsx': 
+		case 'ppt' : 
+		case 'odp' : 
+		case 'pptx': return 'file'; // 'presentation';
+		case 'xls' : 
+		case 'xlsx': 
+		case 'ods' : return 'file'; // 'spreadsheet';
+		case 'txt' : 
+		case 'odt' : 
+		case 'doc' : 
+		case 'docx': return 'textdocument';
+		case 'odg' : return 'file'; // 'vectorgraphics';
+		case 'wmv' : 
+		case 'mpeg': 
+		case 'mpg' : 
+		case 'mov' : 
+		case 'avi' : return 'video';
+		case 'htm' : 
+		case 'html': return 'file'; // 'webpage';
+	}
+	return 'file';
+}
+
+function format_size( $file_path ) {
+	$sizes = array( 'o', 'Ko', 'Mo', 'Go', 'To', 'Po', 'Eo' );
+	$i = 1;
+	$size = filesize( $file_path );
+	while ( $i < count( $sizes ) && ( $size >= 1024 ) ) {
+		$size = $size / 1024;
+		$i++;
+	}
+	return round( $size, 2 ) . ' ' . $sizes[ $i - 1 ];
 }
